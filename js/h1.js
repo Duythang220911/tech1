98a696f78fb697e252655bfd3f8a5eca52c2ef1d
Duythang220911 <121070706+Duythang220911@users.noreply.github.com>
2026-07-13 21:18:56 +0700

    Update h1.js

--- js/h1.js
+++ js/h1.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", () => {
             if (balanceVisible) {
                 balanceValue.innerHTML = `
                     <span class="currency">VND</span>
-                    <span class="amount">16,324,412</span>
+                    <span class="amount">16,324,413</span>
                 `;
 
                 balanceValue.classList.remove(
